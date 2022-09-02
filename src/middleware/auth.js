602da0b8f50e53f4bf8e57809ca8b4cf2a66ece6
Pabitra Ranjan Rout <pabitraranjanrout@gmail.com>
2022-09-02 17:11:47 +0530

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,14 +5,17 @@ const authentication =(req,res,next)=>{
     const authHeader=req.headers.authorization;
     if(!authHeader){
         res.status(401).send("User is not authenticated");
+        return
     }
     const token=authHeader.split(" ")[1];
     try {
         const user=jwt.verify(token,process.env.JWT_SECRET);
+        console.log(user);
         req.user=user;
         next();
     } catch (error) {
         res.status(403).json({msg:error});
+        return;
     }
 }
 module.exports=authentication
